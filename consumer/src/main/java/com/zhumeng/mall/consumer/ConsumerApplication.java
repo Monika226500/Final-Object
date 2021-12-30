5853d7c4905563428e82cb8208ebcef2b6760781
Monika226500 <2435152308@qq.com>
2021-12-30 10:51:43 +0800

    电商网站后台管理系统

--- consumer/src/main/java/com/zhumeng/mall/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/zhumeng/mall/consumer/ConsumerApplication.java
@@ -3,6 +3,9 @@ package com.zhumeng.mall.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author mine
+ */
 @SpringBootApplication
 public class ConsumerApplication {
 
